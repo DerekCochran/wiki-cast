06a1b01d5bfd5c4e0b63e7cd8ca174e10052ff44
Derek Cochran <derekjcochran@gmail.com>
2026-05-08 21:40:14 -0500

    wip

--- bindings/node/tests/play.js
+++ bindings/node/tests/play.js
@@ -4,7 +4,7 @@ const wikiparser = require("wikiparser-node");
 const nativeProto = require(path.join(__dirname, '..', 'build', 'Debug', 'wikiparser-node-c-tokenizer.node'));
 
 wikiparser.config = "enwiki";
-const testString = "{{Infobox person\n| name = John Doe\n| birth_date = {{Birth date|1950|1|1}}\n}}";
+const testString = "[[#Section]]";
 const config = wikiparser.getConfig();
 const jsImpl = wikiparser.parse(testString, {
     config: config
@@ -16,10 +16,14 @@ const nativeImpl = nativeProto.parse(testBuffer, {
 });
 
 if( jsImpl.toString() !== nativeImpl.toString() ) {
+    console.log("jsImpl.toString(): "+ jsImpl.toString());
+    console.log("nativeImpl.toString(): "+ nativeImpl.toString());
     console.error("Error: The string representations of the AST outputs differ between implementations.");
 }
 if (JSON.stringify(jsImpl) === JSON.stringify(nativeImpl)) {
     console.log("Success: Both implementations produce the same AST.");
 } else {
+    console.log("jsImpl JSON: "+ JSON.stringify(jsImpl));
+    console.log("nativeImpl JSON: "+ JSON.stringify(nativeImpl));
     console.error("Error: The AST outputs differ between implementations.");
 }
--- check_sz.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "stringzilla/stringzilla.h"
-int main(void){
-  printf("sz_dynamic_dispatch()=%d\n", sz_dynamic_dispatch());
-  printf("sz_capabilities=%s\n", sz_capabilities_to_string(sz_capabilities()));
-  return 0;
-}
