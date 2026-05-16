163153c31e1f4263d3871302112238288a7fbc08
Derek Cochran <derekjcochran@gmail.com>
2026-05-15 21:25:51 -0500

    Keep for next time.  Will need a true free isntead of a reset

--- src/accum.c
+++ src/accum.c
@@ -30,10 +30,11 @@ Token *accum_get(const Accum *a, size_t i) {
 	return a->tokens[i];
 }
 
+// TODO:  Do a true free and rename this to reset
 void accum_free(Accum *a) {
 	if(!a) return;
-	free(a->tokens);
-	a->tokens= NULL;
+	//free(a->tokens);
+	//a->tokens= NULL;
 	a->count= 0;
-	a->cap= 0;
+	//a->cap= 0;
 }
