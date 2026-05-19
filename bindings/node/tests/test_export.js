eed94da3f449c09aaa0f246d1b79d23c61d034b2
Derek Cochran <derekjcochran@gmail.com>
2026-05-19 06:22:46 -0500

    Testing

--- bindings/node/tests/test_export.js
+++ bindings/node/tests/test_export.js
@@ -58,7 +58,7 @@ async function main(argv) {
   const args = argv.slice(2);
   const options = {
     input: null,
-    start: 1,
+    start: 5000,
     end: 0, // 0 means no limit
   };
 
@@ -141,7 +141,7 @@ async function main(argv) {
 //       console.info(`The files have been copied.  Please run the below commands
 // cd ${path.dirname(__filename)}
 // node test_wikitext.js\n\n`);
-//       process.exit(2);
+       process.exit(2);
     }
 
     if (processed % 10 === 0) {
